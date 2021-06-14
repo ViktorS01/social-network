a081a9a522d688051c96726485baecea9a18974b
ViktorS01 <75483754+ViktorS01@users.noreply.github.com>
2021-06-14 17:16:50 +0400

    init: start project lessons 1 - 10 - first steps

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Article from './Article';
+import Technologies from './Technologies';
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <Article />
+      <Technologies />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Article.js
@@ -0,0 +1,12 @@
+const Article = () => {
+    return(
+      <nav>
+        <ul>
+          <a href="#" > Ссылка раз!</a>
+          <a href="#" > Ссылка два!</a>
+        </ul>
+      </nav>
+    );
+};
+
+export default Article;
