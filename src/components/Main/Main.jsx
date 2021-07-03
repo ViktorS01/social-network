adfb0ef6ba0d2a42c5b95c9a97c5a3d0e24f59d8
ViktorS01 <75483754+ViktorS01@users.noreply.github.com>
2021-07-03 18:28:54 +0400

    fix: change structure directory

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import classes from "./Main.module.css";
+import Post from "./Posts/Post/Post";
 
 const Main =  () => {
     return (
         <div className={classes.main}>
             <img src="https://i.pinimg.com/originals/70/bc/be/70bcbe9be53e7f1a505000827f383914.jpg" alt="BG"/>
+            <Post />
+            <Post />
+            <Post />
+            <Post />
         </div>
     );
 };
---
+++ src/components/Main/Posts/Post/Post.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import classes from "./Post.module.css";
+
+const Post =  () => {
+    return (
+        <div>
+            POST
+            <button>Like!</button>
+        </div>
+    );
+};
+
+export default Post;
