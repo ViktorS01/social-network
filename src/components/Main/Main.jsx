bb3e9bb5079ceda89192330ee223fd27f8ed687b
ViktorS01 <75483754+ViktorS01@users.noreply.github.com>
2021-07-06 19:44:49 +0400

    feat: add props

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -6,10 +6,10 @@ const Main =  () => {
     return (
         <div className={classes.main}>
             <img src="https://i.pinimg.com/originals/70/bc/be/70bcbe9be53e7f1a505000827f383914.jpg" alt="BG"/>
-            <Post />
-            <Post />
-            <Post />
-            <Post />
+            <Post message="It is me!" likeCount="197"/>
+            <Post message="It is my mum!" likeCount="105"/>
+            <Post message="It is my dog, i love you, baby!" likeCount="89"/>
+            <Post message="Breakfast at my work.." likeCount="16"/>
         </div>
     );
 };
--- src/components/Main/Posts/Post/Post.jsx
+++ src/components/Main/Posts/Post/Post.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import classes from "./Post.module.css";
 
-const Post =  () => {
+const Post =  (props) => {
     return (
         <div>
-            POST
+            <div>{props.message}</div>
+            <div>{props.likeCount} like on you photo.</div>
             <button>Like!</button>
         </div>
     );
